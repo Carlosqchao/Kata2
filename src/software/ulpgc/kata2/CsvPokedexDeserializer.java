43b309fd382f660fe423a64e658bea788f43295e
Carlosqchao <carlosruanoramos@gmail.com>
2024-11-10 13:56:01 +0000

    Deserializer Class

--- src/software/ulpgc/kata2/CsvPokedexDeserializer.java
+++ src/software/ulpgc/kata2/CsvPokedexDeserializer.java
@@ -3,9 +3,10 @@ package software.ulpgc.kata2;
 public class CsvPokedexDeserializer implements PokedexDeserializer {
 
     @Override
-    public  Pokedex deserialize(String line){
+    public Pokedex deserialize(String line){
         return deserialize(line.split(";"));
     }
+
 
     private Pokedex deserialize(String[] fields){
         return new Pokedex(toInt(fields[1]),fields[2],toInt(fields[5]),toInt(fields[15]));
